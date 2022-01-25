fb88b28d62d409cae17da0a3231d360a3e89a182
Sharad.Singh <SharadSingh001@yahoo.com>
2022-01-24 21:26:14 -0500

    Add video, new images in photo gallery and update contact info

--- js/index.js
+++ js/index.js
@@ -13,11 +13,51 @@ function getInfo(){
 	console.log(phone);
 	console.log(message);
 
-	var link = "mailto:rishisingh@gmail.com"
+	var link = "mailto:swadeshhomesinc@gmail.com"
              + "?cc=sharadsingh001@yahoo.com"
              + "&subject=" + encodeURIComponent("Swadesh Homes Contact Request")
-             + "&body=" + encodeURIComponent(name + "\n" + email + "\n" + phone + "\n\n" + message)
+             + "&body=" + encodeURIComponent("Name: " + name + "\n" + "Email: " + email + "\n" + "Phone: " + phone + "\n\n" + "Message: " + message)
     ;
     
     window.location.href = link;
+}
+
+function videoFunction() {
+  var video = document.getElementById("SwadeshHomesAdVideo");
+  var btn = document.getElementById("myBtn");
+
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
